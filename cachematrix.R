1451d349658f4531c056e4321cf8e8e88417602d
mrlantan <andruha.qbit@gmail.com>
2015-03-23 00:41:56 +0200

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creating matrixes and calculating inversed ones
 
-## Write a short comment describing this function
+## Following Creates Special Matrix Type Object
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- matrix ()
+    set <- function (y) {
+        x <<- y
+        m <<- matrix ()
+    }
+    get <- function () x
+    setsolve <- function (solve) m <<- solve
+    getsolve <- function () m
+    list (set = set, get = get, 
+          setsolve = setsolve, 
+          getsolve = getsolve)
+    
 }
 
 
-## Write a short comment describing this function
+## Following Calclates an inverse matrix
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if (!is.na(m[1][1])) {
+        message("getting cached data")
+        return (m)
+    }
+    data <- x$get()
+    m <- solve (data, ...)
+    x$setsolve (m)
+    m
 }
